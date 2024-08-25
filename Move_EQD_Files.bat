e05cb8e74d49e89765faed02455e2afbe6ce8325
SaneSlark <radio585@msn.com>
2024-08-25 17:50:11 +0800

    Create Move_EQD_Files.bat

---
+++ Move_EQD_Files.bat
@@ -0,0 +1,14 @@
+﻿@echo off
+setlocal enabledelayedexpansion
+
+:: 创建目录 EQD，如果目录已存在则不会有影响
+if not exist EQD md EQD
+
+:: 直接在 for 循环中处理 dir 命令的输出
+for /f "delims=" %%i in ('dir *.eqd /s /b') do (
+    move "%%i" EQD
+)
+
+:: 输出完成信息并提示用户按任意键退出
+echo Files moved to EQD directory.
+pause
